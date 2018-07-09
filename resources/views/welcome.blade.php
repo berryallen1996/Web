47f7c8fe35327f1f700133c52d3927b68728ef5a
berryallen1996 <pawan@singsys.com>
2018-07-10 00:28:26 +0530

    Update Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel tech
                 </div>
 
                 <div class="links">
