a2c64a7afb8fe117bf217f4a961e93bc2ffcd670
berryallen1996 <pawan@singsys.com>
2018-07-10 01:22:41 +0530

    update

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Pagination Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the paginator library to build
+	| the simple pagination links. You are free to change them to anything
+	| you want to customize your views to better match your application.
+	|
+	*/
+	'reset_password'							=> 'Success! A password reset link has been sent to your work email address.',
+	'logout'									=> 'You have logged out successfully.',
+	'not_found_user' 							=> 'User not found.',
+	'user_block'								=> 'User is blocked.',
+	'successful_login'							=> 'Login successful.',
+	'failed_login'								=> 'Login failed.',
+	'incorrect_email_password'					=> 'Oops, the email address and/or password is incorrect. Please try again',
+	'old_password_invalid'						=> 'Old password is incorrect, please try again.',
+	'password_changed_successfully'				=> 'Your password has been changed successfully.',
+	'password_changed_failed'					=> 'Invalid request, please try again.',
+	'order_cancelled_successfully' 				=> 'Your order has been cancelled.',
+	'order_already_cancelled' 					=> 'Your order has been already cancelled.',
+	'order_cannot_cancelled' 					=> 'You can not cancel your order.',
+	'order_can_cancelled_before_48' 			=> 'Sorry, you can only cancel your order up to 48 ours prior to the delivery time.',
+	'invalid_email_password'					=>	'Sorry, invalid email address or password. Please try again.',
+	'order_received' 							=> "Order Confirmed \n Enjoy your meal",
+	'dish_added_successfully'					=> 'Dish has been added successfully',
+	'checkout_procces_empty_cart'				=> 'There is no dishes selected in your cart.',
+	'dish_remove_successfully'					=>	"Dish has been removed successfully.",
+	'user_blocked'								=> 'You can not access your account, Please contact to DishDash Admin.',
+	'already_registered'						=> 'This email has already been registered.',
+	'search_not_found'							=> 'We\'re sorry, your search found no results.'
+
+];
